d7b8503b0dabd1506cfc5142aa9c65e600cf772e
trannhan2019 <53633928+trannhan2019@users.noreply.github.com>
2021-01-08 16:22:56 +0700

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('username')->unique();
             $table->string('email')->unique();
-            $table->string('referral_token')->unique();
+            //$table->string('referral_token')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
